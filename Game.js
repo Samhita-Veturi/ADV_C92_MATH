49079cf9b61249aaeaf532c2e46825f882d7d590
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-01-16 15:40:12 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,8 +1,8 @@
 var Ply_1 = localStorage.getItem("Player_1");
 var Ply_2 = localStorage.getItem("Player_2");
 
-Ply_1Score = 0;
-Ply_2Score = 0;
+var Ply_1Score = 0;
+var Ply_2Score = 0;
 
 document.getElementById("P1_Name").innerHTML= Ply_1 + " : ";
 console.log(Ply_1);
@@ -35,33 +35,33 @@ function Check(){
     Get_A = document.getElementById("word_display").value;
     if(Get_A==Actual_Ans){
         if(A_Turn == "Player1"){
-        Up_Ply1_S = Ply_1Score + 1;
+        Ply_1Score = Ply_1Score + 1;
         document.getElementById("P1_S").innerHTML = Ply_1Score;
         }
         else{
-            Up_Ply2_S = Ply_2Score + 1;
-            document.getElementById("P2_S").innerHTML = Ply_1Score;
+            Ply_2Score = Ply_2Score + 1;
+            document.getElementById("P2_S").innerHTML = Ply_2Score;
         }
-        if(Q_Turn=="Player2"){
-            Q_Turn = "Player1";
-            document.getElementById("Ques_P").innerHTML = "Question Turn - " + Ply_1;
-        }
-        else{
-            Q_Turn = "Player2";
-            document.getElementById("Ques_P").innerHTML = "Question Turn - " + Ply_2;
-        }
-        if(A_Turn == "Player1"){
-            A_Turn = "Player2";
-            document.getElementById("Ans_P").innerHTML = "Answer Turn - " + Ply_2;
-        }
-        else{
-            A_Turn = "Player1";
-            document.getElementById("Ans_P").innerHTML = "Answer Turn - " + Ply_1;
-        }
-        Label = "<br><label id='Cheesy_Dip'>" + "Correct Answer!" + "</label>";
+    Label = "<br><label id='Cheesy_Dip'>" + "Correct Answer!" + "</label>";
     }
     else{
         Label = "<br><label id='Cheesy_Dip'>" + "Sorry, Wrong Answer!" + "</label>";
+    }
+    if(Q_Turn=="Player2"){
+        Q_Turn = "Player1";
+        document.getElementById("Ques_P").innerHTML = "Question Turn - " + Ply_1;
+    }
+    else{
+        Q_Turn = "Player2";
+        document.getElementById("Ques_P").innerHTML = "Question Turn - " + Ply_2;
+    }
+    if(A_Turn == "Player1"){
+        A_Turn = "Player2";
+        document.getElementById("Ans_P").innerHTML = "Answer Turn - " + Ply_2;
+    }
+    else{
+        A_Turn = "Player1";
+        document.getElementById("Ans_P").innerHTML = "Answer Turn - " + Ply_1;
     }
     document.getElementById("Output").innerHTML = Label;
 }
